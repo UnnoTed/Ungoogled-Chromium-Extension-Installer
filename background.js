7a9450705d4b73f55ec91d064db5cbd150129167
UnnoTed <unnotedx@gmail.com>
2018-07-23 13:49:21 -0300

    Fix invalid extension's id when query is present in the url

--- background.js
+++ background.js
@@ -7,7 +7,14 @@ chrome.contextMenus.create({
 
       if (result) {
         const version = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
-        const extension_id = tabs[0].url.split("/")[6];
+        let extension_id = tabs[0].url.split("/")[6];
+
+        // removes query
+        const q = extension_id.indexOf("?");
+        if (q !== -1) {
+          extension_id = extension_id.substr(0, q);
+        }
+
         const url = `https://clients2.google.com/service/update2/crx?response=redirect&prodversion=${version}&x=id%3D${extension_id}%26installsource%3Dondemand%26uc`;
         window.open(url, "_blank");
       }
